a3f6e637380d899ac3243a29f74d4adde91363fe
Michał Terczyński <mterczynski1@gmail.com>
2024-06-08 02:14:42 +0200

    Add mine-sweeper build

---
+++ mine-sweeper/precache-manifest.ecfd5db0a775783357deff88d56e7ffa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e55461c793d5ecc735ef",
+    "url": "/static/js/main.e55461c7.chunk.js"
+  },
+  {
+    "revision": "9e76ea400f1b13106622",
+    "url": "/static/js/1.9e76ea40.chunk.js"
+  },
+  {
+    "revision": "e55461c793d5ecc735ef",
+    "url": "/static/css/main.338c3893.chunk.css"
+  },
+  {
+    "revision": "6c80c681bb2300ea9d07411e3945b415",
+    "url": "/index.html"
+  }
+];
